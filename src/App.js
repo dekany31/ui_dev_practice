130813429b00c503c455b8837836154c7970671e
dekany31 <66486271+dekany31@users.noreply.github.com>
2021-03-18 22:30:16 +0100

    implementation

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="App-nav">
+        <i className="bi bi-list" id="burger"></i>
+      </nav>
+      <div className="content">
+        {/* <h1>Find Your <br/> Dream Job <br/> with Us!</h1> */}
+        <h1>Find Your Dream Job with Us</h1>
+        <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat minim veniam, 
+        quis nostrud exercitation consectetur. 
+        </p>
+        <button>A P P L Y</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
